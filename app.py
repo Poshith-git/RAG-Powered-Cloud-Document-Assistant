9868c96548761203efe24a151afc53bd0b4e58dc
Poshith <poshithreddyr@gmail.com>
2026-02-13 23:11:20 +0530

    Day 1: Clean setup and base Streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cloud-Based RAG Document Assistant")
+
+st.title("📄 Cloud-Based RAG Document Assistant")
+st.write("Day 1 Setup Successful 🚀")
+
+st.info("PDF upload and AI processing will be added in Day 2.")
